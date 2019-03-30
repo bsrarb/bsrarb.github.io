239616ddc24eb20b11aaeb774b9b5ad90ca544e7
bsrarb <busraarabaci@gmail.com>
2019-03-30 22:15:39 +0300

    Download resume

--- download.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'busra_arabaci_resume.pdf'
-if (file_exists($file) && is_readable($file) && preg_match('/\.pdf$/',$file)) {
-	header('Content-Type: application/pdf');
-	header("Content-Disposition: attachment; filename=\"$file\"");
-	readfile($file);
-	}
-}
-?> 
