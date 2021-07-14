2853d3077b473fd8529ca064d046f7730f8d93b3
Büşra Arabacı <busraarabaci@gmail.com>
2021-07-14 13:04:20 +0300

    Update app.js

--- webxrecommerce/src/app.js
+++ webxrecommerce/src/app.js
@@ -114,7 +114,7 @@ class App{
         this.gestures.addEventListener( 'rotate', (ev)=>{
             //      sconsole.log( ev ); 
             if (ev.initialise !== undefined){
-                self.startQuaternion = self.knight.object.quaternion.clone();
+                self.startQuaternion = self.chair.object.quaternion.clone();
             }else{
                 self.chair.object.quaternion.copy( self.startQuaternion );
                 self.chair.object.rotateY( ev.theta );
